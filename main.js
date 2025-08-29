256e7d3dcf664f258b1f42cc2916b51c66cf03a5
Isabella Polania <123099396+izziepolania0@users.noreply.github.com>
2025-08-28 20:41:42 -0500

    initial commit

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
-// Step 1 - Find all the elements we need to interact with
-const num1Input = document.getElementById('number1')
-const num2Input = document.getElementById('number2')
+//Step 1: Find all the elements to interact with
+const numb1Input = document.getElementById('number1')
+const numb2Input = document.getElementById('number2')
 const operatorSelect = document.getElementById('operator')
 const calculateButton = document.getElementById('calculateButton')
 const resultSpan = document.getElementById('result')
 
-// Step 2 - Add an event listener to the button to call a function when clicked
 calculateButton.addEventListener('click', calculate)
 
-function calculate() {
-    const num1 = parseFloat(num1Input.value)
-    const num2 = parseFloat(num2Input.value)
+function calculate(){
+
+    const num1 = parseFloat(numb1Input.value)
+    const num2 = parseFloat(numb2Input.value)
     const operator = operatorSelect.value;
 
     let result
-    if( operator === "+") {
+    if(operator=== "+"){
+
         result = num1 + num2
     }
-    
-    // Step 3 - update the result span with the result of the calculation
+
+    else if(operator=== "-"){
+
+        result = num1 - num2
+    }
+
+     else if(operator=== "*"){
+
+        result = num1 * num2
+    }
+
+     else if(operator=== "/"){
+
+        result = num1 / num2
+    }
+
+
+
+
     resultSpan.innerText = result;
+
 }
